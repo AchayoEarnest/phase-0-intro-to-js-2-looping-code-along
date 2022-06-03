10cdf3b3186b060c05f6d503daeb24a7b4169826
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-06-03 14:44:23 +0300

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // Code your solutions in this file
-let namesArray = ["Guadalupe", "Ollie", "Aki"];
+var namesArray = ["Guadalupe", "Ollie", "Aki"];
+var event = "surpriseGift"
 
 function writeCards(namesArray, event){
     let cards = [];
-    for(let i = 0; i < namesArray.length; i++ )
-    cards.push(`Thank you, ${namesArray[i]}, for the wonderful birthday gift!,`);
+    for(var i = 0; i < namesArray.length; i++){
+        cards.push(`Thank you, ${namesArray[i]}, for the wonderful birthday gift!,`);
+    }
 
     console.log(cards);
 }
